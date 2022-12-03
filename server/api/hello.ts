0e1befe6bbb91cbfd7de3ca042fb37e3d3c527b5
nikita-t1 <26936278+nikita-t1@users.noreply.github.com>
2022-12-03 18:51:40 +0100

    api/hello

--- server/api/hello.ts
+++ server/api/hello.ts
@@ -1,5 +1,6 @@
-export default defineEventHandler(() => {
-    'Hello World!';
+export default defineEventHandler((event) => {
     console.log(process.env)
+    console.log("-----------------")
+    return 'Hello World!';
 
 })
