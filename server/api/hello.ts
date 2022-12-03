e13e8a4979c62e0a1d96c9d8d935ab80dbbcbdad
nikita-t1 <26936278+nikita-t1@users.noreply.github.com>
2022-12-03 18:57:54 +0100

    api/hello  process.env

--- server/api/hello.ts
+++ server/api/hello.ts
@@ -1,6 +1,8 @@
 export default defineEventHandler((event) => {
     console.log(process.env)
     console.log("-----------------")
+    console.log("Meine SupaBase URL ist:")
+    console.log(".SUPABASE_URL=" + process.env.SUPABASE_URL)
     return 'Hello World!';
 
 })
